d54bd7bb038d640e35555aece0223b57f3129966
Shruti Sharma <shruti@thebrainburners.io>
2026-03-23 12:40:11 +0530

    updated frontend

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
 
   return (
     <section className="flex flex-col items-center justify-between sm:justify-start pt-10 px-4 sm:px-6 text-center min-h-screen sm:min-h-auto ">
-  <div>
+  <div className="flex flex-col items-center justify-center">
       < h1 className="hidden sm:block text-4xl md:text-6xl font-medium leading-tight max-w-4xl">
         Find Out Why Your
         <span className="font-semibold text-[#F38400]"> Website</span>
@@ -96,10 +96,10 @@ const HeroSection = () => {
       </p>
     </div>
 
-<div className="mt-6 w-full flex justify-center items-center">
+<div className="mt-0 sm:mt-6 w-full flex justify-center items-center">
         <img 
   src="/astro.svg" 
-  className="block max-w-md" 
+  className="block max-w-lg" 
 />
       </div>
 
