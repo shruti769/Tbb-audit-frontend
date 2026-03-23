8e862ed85491629d060b47397e8399f7ad3ba0dd
Shruti Sharma <shruti@thebrainburners.io>
2026-03-23 12:03:18 +0530

    updated frontend

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-// export const BASE_URL="http://localhost:5000"
-export const BASE_URL="https://tbb-audit-backend-production.up.railway.app"
+export const BASE_URL="http://localhost:5000"
+// export const BASE_URL="https://tbb-audit-backend-production.up.railway.app"
