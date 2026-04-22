2b2f4b30c2a8fc999259db8c5bc749224783403d
Shruti Sharma <shruti@thebrainburners.io>
2026-04-22 18:34:40 +0530

    bugs fixed

--- src/ReportPage.jsx
+++ src/ReportPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import ReportHero from "./components/ReportPageComponents/ReportHero";
 import ReportHeader from "./components/ReportPageComponents/ReportHeader";
